c0c34e4075f42cb76b85a5accf79bf13efff4218
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-04-08 19:29:39 +0900

    [finsh] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5278f22584d19c2f18bf2d11f75610cd1d60c1fad5e8964e2faa62451f627a97d8858c713bb2944660ecf7818fc4d14013a24d727dc946d648230ab9ad22bae4'
